85dcb2c065ee943c38b73557cf6b58643fef78bf
Trent Buckholz <trentjbuckholz@gmail.com>
2025-03-13 19:48:09 -0700

    Initial commit

---
+++ src/CartSlice.jsx
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const plant = action.payload;
+      const plantToReplace = state.items.find(item => item.name === plant.name);
+      if (plantToReplace) {
+        plantToReplace.quantity += 1
+        return {...state};
+      } else {
+        return {
+            ...state,
+            items: [...state.items, {...plant, quantity: 1}]
+          };
+      }
+    },
+    removeItem: (state, action) => {
+      const plantToRemove = action.payload;
+      return {
+        ...state,
+        items: state.items.filter(plant => plant.name !== plantToRemove.name)
+      };
+    },
+    updateQuantity: (state, action) => {
+      const {plant, quantity} = action.payload;
+      return {
+        ...state,
+        items: [...state.items.filter(item => item.name !== plant.name), {...plant, quantity: quantity}]
+      };
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
